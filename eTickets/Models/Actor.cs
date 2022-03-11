1878a04e5331e9679423814605d0eddbf3f98baf
Mohd Mustkeem <mohdmustkeem78624@gmail.com>
2022-03-11 17:37:47 +0530

    Added Actor And Producer

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+    }
+}
