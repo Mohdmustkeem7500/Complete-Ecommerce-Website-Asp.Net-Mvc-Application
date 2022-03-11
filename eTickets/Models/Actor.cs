7de5551c844316395e4b29427d1b8f50cc8dd6d1
Mohd Mustkeem <mohdmustkeem78624@gmail.com>
2022-03-11 19:57:19 +0530

    Added eTickets Model and Data

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -15,5 +15,11 @@ namespace eTickets.Models
         public string FullName { get; set; }
         public string Bio { get; set; }
 
+
+        //Relationships
+
+        public List<Actor_Movie> Actor_Movies { get; set; }
+
+
     }
 }
--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -14,5 +14,9 @@ namespace eTickets.Models
         public string ProfilePictureURL { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; }
+
+        //Relationships
+
+        public List<Movie> Movies { get; set; }
     }
 }
